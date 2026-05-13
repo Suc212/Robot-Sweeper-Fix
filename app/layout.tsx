8a9389bfb6d0401897a9f33665ad842c4e45c26e
suc <omotayo.olukale@gmail.com>
2026-05-13 15:38:56 +0100

    added clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,15 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wqia2a8hed");
+          `}
+        </Script>
       </head>
       <body className={`font-sans antialiased`}>
         <noscript>
